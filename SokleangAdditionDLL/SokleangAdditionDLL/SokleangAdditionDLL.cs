5832ca3997c352cb6f10dd09b3580a071887a740
sokleang kry <42300316+SokleangKry@users.noreply.github.com>
2018-09-08 00:01:59 +0700

    Add files via upload

---
+++ SokleangAdditionDLL/SokleangAdditionDLL/SokleangAdditionDLL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class SokleangAdditionDLL
+{
+    public static int Addition(int a, int b) => a + b;
+}
+
